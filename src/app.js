9c0c90c6658f42a2d15d482117238fcf4b1818c4
JAPGarmando <jarmando_gama@hotmail.com>
2022-09-26 18:00:17 -0500

    chore: no comments on app

--- src/app.js
+++ src/app.js
@@ -7,24 +7,7 @@ app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
+// Set the routes from routes/index
 app.use(`/`, routes);
-
-/**
- * FIX ME!
- * @returns contract by id
- */
-// app.get('/contracts/:id',getProfile ,async (req, res) =>{
-//     const {Contract} = req.app.get('models')
-//     const {id} = req.params
-//     console.info("ID =>", id);
-//     console.info("PROFILE =>", req.profile);
-//     const {dataValues: profileData } = req.profile;
-//     const contract = await Contract.findOne({where: {
-//         id: 1,
-//         ContractorId: profileData.id
-//     }})
-//     if(!contract) return res.status(404).end()
-//     res.json(contract)
-// });
 
 module.exports = app;
